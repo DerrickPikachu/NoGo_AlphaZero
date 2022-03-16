a4bcbe1081d4cc241698c64ba26e02487d0f2369
Jui Te Chin <pikachin.cs10@nycu.edu.tw>
2022-03-16 10:33:18 +0000

    refactor self_play_loop

--- mcts/actor.cpp
+++ mcts/actor.cpp
@@ -6,19 +6,37 @@
 #include "proto/trajectory.pb.h"
 #include "self_play.h"
 
+// void self_play_loop(player& black, player& white) {
+//     while (true) {
+//         episode game;
+//         game.open_episode(black.name() + ":" + white.name());
+//         while (true) {
+//             agent& who = game.take_turns(black, white);
+//             action move = who.take_action(game.state());
+//             if (game.apply_action(move) != true) break;
+//             if (who.check_for_win(game.state())) break;
+//         }
+//         agent& win = game.last_turns(black, white);
+//         game.close_episode(win.role());
+//         std::cout << win.role() << std::endl;
+//     }
+// }
+
 void self_play_loop(player& black, player& white) {
+    EngineInterface* engine = new SelfPlayEngine(&black, &white);
     while (true) {
-        episode game;
-        game.open_episode(black.name() + ":" + white.name());
-        while (true) {
-            agent& who = game.take_turns(black, white);
-            action move = who.take_action(game.state());
-            if (game.apply_action(move) != true) break;
-            if (who.check_for_win(game.state())) break;
+        engine->init_game(new episode());
+        engine->open_episode();
+        while(true) {
+            action move = engine->next_action();
+            if (engine->apply_action(move) == false) break;
+            engine->store_transition(move);
         }
-        agent& win = game.last_turns(black, white);
-        game.close_episode(win.role());
-        std::cout << win.role() << std::endl;
+        agent& winner = engine->close_episode();
+        std::string raw_trajectory = 
+            ((SelfPlayEngine*)engine)->get_trajectory();
+        // TODO: send the raw trajectory to learner
+        std::cout << winner.role() << std::endl;
     }
 }
 
