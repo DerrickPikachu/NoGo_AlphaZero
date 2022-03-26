8268ba413835f702925ec40afecff347cb5fe050
Jui Te Chin <pikachin.cs10@nycu.edu.tw>
2022-03-26 11:42:24 +0000

    add replay buffer into main routine

--- train/learner.py
+++ train/learner.py
@@ -1,8 +1,12 @@
 from trajectory_socket import TrajectoryServer
+from replay_buffer import ReplayBuffer, Transition
 import trajectory_pb2 as trajectory
+
+import torch
 
 
 def self_play_loop(actor_socket: TrajectoryServer):
+    replay_buffer = ReplayBuffer(300)
     # file = open('test_trajectory', 'wb')
     while True:
         byte_int = actor_socket.recv(4)
@@ -15,9 +19,15 @@ def self_play_loop(actor_socket: TrajectoryServer):
         print("receive raw data len: {}".format(len(raw)))
         parsed_trajectory = trajectory.trajectory()
         parsed_trajectory.ParseFromString(raw)
+        for tran in parsed_trajectory.transitions:
+            state_tensor = torch.tensor(tran.state)
+            transition = Transition(state_tensor, tran.action_id, tran.reward)
+            replay_buffer.append(transition)
         # file.write(parsed_trajectory.SerializeToString())
         print("num_transition: ", len(parsed_trajectory.transitions))
         print("last reward: ", parsed_trajectory.transitions[-1].reward)
+        print("replay buffer size: ", len(replay_buffer))
+        print("-----------------------------------------")
 
 
 if __name__ == "__main__":
