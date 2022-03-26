9f0b67edb9fe3cd45ff856060e819ec9e9af9587
Jui Te Chin <pikachin.cs10@nycu.edu.tw>
2022-03-26 11:42:07 +0000

    replay buffer refactor

--- train/replay_buffer.py
+++ train/replay_buffer.py
@@ -1,14 +1,18 @@
 import abc
+from collections import deque, namedtuple
+from sys import maxsize
 import torch
 import random
 
 
-class Transition:
-    def __init__(self, state:torch.Tensor, action:int, 
-                 reward:float) -> None:
-        self.state = state
-        self.action = action
-        self.reward = reward
+# class Transition:
+#     def __init__(self, state:torch.Tensor, action:int, 
+#                  reward:float) -> None:
+#         self.state = state
+#         self.action = action
+#         self.reward = reward
+Transition = namedtuple('Transition', 
+                        ('state', 'action', 'reward'))
 
 
 class BufferInterface(abc.ABC):
@@ -33,16 +37,18 @@ class BufferInterface(abc.ABC):
 class ReplayBuffer(BufferInterface):
     def __init__(self, buffer_size: int) -> None:
         super().__init__(buffer_size)
+        # TODO: use deque to be the buffer
         self.size = buffer_size
-        self.buffer = list()
+        self.buffer = deque([], maxlen=buffer_size)
         
     def append(self, transition:Transition) -> None:
         self.buffer.append(transition)
         if len(self.buffer) > self.size:
-            self.buffer.pop(0)
+            self.buffer.popleft()
             
     def sample(self, num_samples) -> list:
-        return random.sample(range(len(self.buffer)), num_samples)
+        # TODO: return a list of transition, not index array
+        return random.sample(self.buffer, num_samples)
     
     def __len__(self) -> int:
         return len(self.buffer)
